9a47605d1ebebbd216ada196c4dd99e67a68678f
x <x@y.z>
2026-05-11 22:11:44 -0700

    timeout

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ s=requests.Session();
 urls=['/mgCalendarMonthView.aspx?GL=1&bcr=1&M=1&Y=2026','/mgCalendarMonthView.aspx?GL=1&bcr=1&month=1&year=2026','/mgCalendarMonthView.aspx?bcr=1&date=2026-01-28','/mgCalendarMonthView.aspx?GL=1&bcr=1']
 os.makedirs('dump',exist_ok=True)
 for i,u in enumerate(urls):
- r=s.get(base+u); print(u,r.status_code,len(r.content),r.url)
+ r=s.get(base+u,timeout=20); print(u,r.status_code,len(r.content),r.url)
  open('dump/cal%d.html'%i,'wb').write(r.content)
